00e73eea18153d9ab6faf5d3a7197774bfc8963d
miura-t-bp <miura_taiki_bp@tenso.com>
2025-02-21 14:00:30 +0900

    dev環境ごとに接続先DBを切り替えることができるよう修正

--- shellapp/scripts/db/get_file_by_domain.sh
+++ shellapp/scripts/db/get_file_by_domain.sh
@@ -11,8 +11,18 @@ else
     dev_num=$(echo "$domain" | sed -e 's/[^0-9]//g')
     dev_num=$((dev_num))
 
+    # pre-prdを使用しているdev環境の番号
+    pre_prd_dev_numbers=(1 2 3 4 6 8 9 10 11 12 13 18)
+
+    for num in "${pre_prd_dev_numbers[@]}"; do
+        if [[ "$num" -eq "$dev_num" ]]; then
+            is_pre_prd=true
+            break
+        fi
+    done
+
     # dev環境の番号が13以下の場合pre-prd、それ以外はstg
-    if [[ $dev_num -le 13 ]]; then
+    if [[ "$is_pre_prd" -eq true ]]; then
         echo "get_data_from_pre_prd.sh"
     else
         echo "get_data_from_stg.sh"
